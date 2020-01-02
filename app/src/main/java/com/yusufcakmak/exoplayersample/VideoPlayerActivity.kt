68bedb62ecb683a904b6205f20d6d156b7b8d095
luanxiaohei <18734184215@163.com>
2020-01-02 13:57:37 +0800

    HelloWorld Exoplayer

--- app/src/main/java/com/yusufcakmak/exoplayersample/VideoPlayerActivity.kt
+++ app/src/main/java/com/yusufcakmak/exoplayersample/VideoPlayerActivity.kt
@@ -4,18 +4,23 @@ import android.app.Activity
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
-import kotlinx.android.synthetic.main.activity_video_player.*
 
-class VideoPlayerActivity : Activity() {
-
+class VideoPlayerActivity : AppCompatActivity() {
+    //创建一个SimpleExoPlayer实例 用于视频和用户输入
     private lateinit var player: SimpleExoPlayer
+    // medirSource 媒体数据源工厂创建方法
     private lateinit var mediaDataSourceFactory: DataSource.Factory
+    // 播放的目标视图
+    private lateinit var playerView:PlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +30,14 @@ class VideoPlayerActivity : Activity() {
     private fun initializePlayer() {
 
         player = ExoPlayerFactory.newSimpleInstance(this)
+        playerView = findViewById(R.id.playerView)
         mediaDataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "mediaPlayerSample"))
-
+       // 创建一个常规媒体文件的播放源
         val mediaSource = ProgressiveMediaSource.Factory(mediaDataSourceFactory).createMediaSource(Uri.parse(STREAM_URL))
-
         with(player) {
             prepare(mediaSource, false, false)
-            playWhenReady = true
+            playWhenReady
+            repeatMode
         }
         playerView.setShutterBackgroundColor(Color.TRANSPARENT)
         playerView.player = player
@@ -45,7 +51,6 @@ class VideoPlayerActivity : Activity() {
 
     public override fun onStart() {
         super.onStart()
-
         if (Util.SDK_INT > 23) initializePlayer()
     }
 
